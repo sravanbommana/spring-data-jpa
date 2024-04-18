28ff149e153ae289b5c95757bae47ca821e86265
Sravan Kumar <bommana@iloguest.org>
2024-04-18 12:48:34 +0530

    jpa changes

---
+++ src/main/java/com/springdatajpa/springboot/entity/Product.java
@@ -0,0 +1,107 @@
+package com.springdatajpa.springboot.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+
+@Entity
+@Table(
+		name="products", 
+		schema="ecommerce",
+		uniqueConstraints = {
+				@UniqueConstraint(
+						name = "sku_unique",
+						columnNames = "sku"
+				)
+		}
+)
+public class Product {
+	@jakarta.persistence.Id
+	private Long Id;
+	private String sku;
+	@Column(name="product_name", nullable=false)
+	private String name;
+	private String description;
+	private BigDecimal price;
+	private boolean active;
+	private String imageUrl;
+	private LocalDateTime dateCreated;
+	private LocalDateTime lastUpdated;
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public String getSku() {
+		return sku;
+	}
+
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public LocalDateTime getDateCreated() {
+		return dateCreated;
+	}
+
+	public void setDateCreated(LocalDateTime dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+
+	public LocalDateTime getLastUpdated() {
+		return lastUpdated;
+	}
+
+	public void setLastUpdated(LocalDateTime lastUpdated) {
+		this.lastUpdated = lastUpdated;
+	}
+
+}
